fbaa1ccb6ba02a2b5517307deaa269a428265dd1
Oleksandr Buhai <oleksandr.buugay@gmail.com>
2022-11-29 20:07:29 +0100

    Components

---
+++ src/ToDoList.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+type ToDoListPropsType = {
+    title:string
+  tasks:Array<TaskType>
+}
+type TaskType = {
+    id:number
+    title:string
+    isDone:boolean
+}
+
+export const ToDoList = (props:ToDoListPropsType) => {
+    return (
+        <div className="App">
+            <div>
+                <h3>What to learn</h3>
+                <div>
+                    <input/>
+                    <button>+</button>
+                </div>
+                <ul>
+                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
+                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
+                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
+                </ul>
+                <div>
+                    <button>All</button>
+                    <button>Active</button>
+                    <button>Completed</button>
+                </div>
+            </div>
+        </div>)
+}

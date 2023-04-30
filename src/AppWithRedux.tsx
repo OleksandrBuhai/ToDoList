768d92723228f592358fe3317b4ee8b1340f3d75
Oleksandr Buhai <oleksandr.buugay@gmail.com>
2023-04-30 23:15:04 +0200

    11
    idk why this don'r coomit whith ohter files

--- src/AppWithRedux.tsx
+++ src/AppWithRedux.tsx
@@ -13,109 +13,62 @@ import { useCallback } from 'react';
 export type FilterType = 'all' | 'active' | 'completed'
 
 export type TasksStateType = {
-    [key:string] : Array<TaskType>
+    [key: string]: Array<TaskType>
 }
-export  type TodoListType = {
+export type TodoListType = {
     id: string,
     title: string,
     filter: FilterType
 }
 
-function AppWithRedux() {
+const AppWithRedux = () => {
 
-
-    const todolists = useSelector<AppRootStateType,TodoListType[]>(state=>state.todolists)
-    const tasks = useSelector<AppRootStateType,TasksStateType>(state=>state.tasks)
+    const todolists = useSelector<AppRootStateType, TodoListType[]>(state => state.todolists)
+    const tasks = useSelector<AppRootStateType, TasksStateType>(state => state.tasks)
     const dispatch = useDispatch()
-    
 
-    const addTodoList = useCallback((title: string)=> {
+
+    const addTodoList = useCallback((title: string) => {
         let action = title
         dispatch(AddToDoListAC(action))
-    },[dispatch])
+    }, [dispatch])
 
-    function deleteTodoList(id: string) {
+    const deleteTodoList = useCallback((id: string) => {
         dispatch(RemoveTodolistAC(id))
-    }
+    }, [dispatch])
 
-    function changeTodoListTitle(id: string, newTitle: string) {
-     /*    const todolist= todolists.find(t=> t.id === id)
-        if (todolist){
-            todolist.title = newTitle
-            setTodolists([...todolists])
-        } */
+    const changeTodoListTitle = useCallback((id: string, newTitle: string) => {
         dispatch(ChangeTodolistAC(id, newTitle))
-    }
+    }, [dispatch])
 
-    function filtredTask( todoListId: string,value: FilterType) {
-      /*   let todoList = todolists.find(el => el.id === todoListId)
-        if (todoList) {
-            todoList.filter = value
-            setTodolists([...todolists])
-        } */
-        dispatch(ChangeTodolistFilterAC(todoListId,value))
-    }
+    const filtredTask = useCallback((todoListId: string, value: FilterType) => {
+        dispatch(ChangeTodolistFilterAC(todoListId, value))
+    }, [dispatch])
 
+    const removeTask = useCallback((id: string, todoListId: string) => {
+        dispatch(removeTaskAC(id, todoListId))
+    }, [dispatch])
 
+    const addTask = useCallback((title: string, todoListId: string) => {
+        dispatch(addTaskAC(title, todoListId))
+    }, [dispatch])
 
+    const changeTaskStatus = useCallback((taskId: string, isDone: boolean, todoListId: string) => {
+        dispatch(changeTaskStatusAC(taskId, isDone, todoListId))
+    }, [dispatch])
 
-    
-    function removeTask(id: string, todoListId: string) {
-  /*       let todolistTasks = tasks[todoListId]
-
-        tasks[todoListId] = todolistTasks.filter(tasks => id != tasks.id)
-        setTasks({...tasks}) */
-        dispatch(removeTaskAC(id,todoListId))
-    }
-
-    function addTask(title: string, todoListId: string)  {
-      /*   let newTask = {id: v1(), title: task, isDone: false}
-        let todolistTask = tasks[todoListId]
-
-        tasks[todoListId] = [newTask, ...todolistTask]
-        setTasks({...tasks}) */
-        dispatch(addTaskAC(title,todoListId))
-    }
-
-    function changeTaskStatus(taskId: string, isDone: boolean, todoListId: string) {
-    /*     let todolistTask = tasks[todoListId]
-        let task = todolistTask.find(t => t.id === taskId)
-        if (task) {
-            task.isDone = isDone
-            setTasks({...tasks})
-        } */
-        dispatch(changeTaskStatusAC(taskId,isDone,todoListId))
-    }
-
-   
-    
-    function changeTaskTitle(taskId: string, newValue: string, todoListId: string) {
-      /*   let todolistTask = tasks[todoListId]
-        let task = todolistTask.find(t => t.id === taskId)
-        if (task) {
-            task.title = newValue
-            setTasks({...tasks})
-        } */
-        dispatch(ChangeTaskTitleAC(taskId,newValue,todoListId))
-    }
+    const changeTaskTitle = useCallback((taskId: string, newValue: string, todoListId: string) => {
+        dispatch(ChangeTaskTitleAC(taskId, newValue, todoListId))
+    }, [dispatch])
 
     return (
         <div className={'App'}>
-            <AddItemForm addItem={addTodoList}/>
+            <AddItemForm addItem={addTodoList} />
             {todolists?.map(t => {
 
-                let allTodolistTasks = tasks[t.id]
-                let taskForToDoList = allTodolistTasks
-                if (t.filter === 'active') {
-                    taskForToDoList = allTodolistTasks.filter(task => task.isDone === false)
-                }
-                if (t.filter === 'completed') {
-                    taskForToDoList = allTodolistTasks.filter(task => task.isDone === true)
-                }
-
-                return <ToDoList key={t.id} title={t.title} tasks={taskForToDoList} removeTask={removeTask}
-                                 filtredTask={filtredTask} addTask={addTask} changeTaskStatus={changeTaskStatus}
-                                 filter={t.filter} id={t.id} deleteTodoList={deleteTodoList} changeTaskTitle={changeTaskTitle} changeTodoListTitle={changeTodoListTitle}/>
+                return <ToDoList key={t.id} title={t.title} tasks={tasks[t.id]} removeTask={removeTask}
+                    filtredTask={filtredTask} addTask={addTask} changeTaskStatus={changeTaskStatus}
+                    filter={t.filter} id={t.id} deleteTodoList={deleteTodoList} changeTaskTitle={changeTaskTitle} changeTodoListTitle={changeTodoListTitle} />
             })}
         </div>
     )
--- src/components/EditableSpan/EditableSpan.tsx
+++ src/components/EditableSpan/EditableSpan.tsx
@@ -1,34 +1,34 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, { ChangeEvent, memo, useState } from 'react';
 
 type EditableSpanPropsType = {
-    title: string
-    onChange: (title:string)=>void
+  title: string
+  onChange: (title: string) => void
 }
 
-const EditableSpan = (props: EditableSpanPropsType) => {
+const EditableSpan = memo((props: EditableSpanPropsType) => {
 
-    const [editMode, setEditMode] = useState<boolean>(false)
-    const [title, setTitle] = useState(props.title)
+  const [editMode, setEditMode] = useState<boolean>(false)
+  const [title, setTitle] = useState(props.title)
 
-const activateEditMode = () => {
-  setEditMode(true)
+  const activateEditMode = () => {
+    setEditMode(true)
     setTitle(props.title)
-}
-const activateViewMode = () => {
-  setEditMode(false)
+  }
+  const activateViewMode = () => {
+    setEditMode(false)
     props.onChange(title)
-}
+  }
 
-const onChangeTitleHandler = (e: ChangeEvent<HTMLInputElement>) => {
-  setTitle(e.currentTarget.value)
-}
+  const onChangeTitleHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.currentTarget.value)
+  }
 
-    return (
-        <>
-            {editMode ? <input  onBlur={activateViewMode} onChange={onChangeTitleHandler} value={title} autoFocus/> :
-                <span onDoubleClick={activateEditMode}>{title}</span>}
-        </>
-    );
-};
+  return (
+    <>
+      {editMode ? <input onBlur={activateViewMode} onChange={onChangeTitleHandler} value={title} autoFocus /> :
+        <span onDoubleClick={activateEditMode}>{title}</span>}
+    </>
+  );
+});
 
 export default EditableSpan;

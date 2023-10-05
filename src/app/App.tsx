a88b75dc81c1c0a4b653d0fdefafc7383da09466
Oleksandr Buhai <oleksandr.buugay@gmail.com>
2023-10-04 19:32:49 -0600

    Move from Redux to RTK

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,80 +1,78 @@
-import React, {useEffect} from 'react'
-import './App.css'
-import {TodolistsList} from '../features/TodolistsList/TodolistsList'
+import React, { useCallback, useEffect } from "react";
+import "./App.css";
+import { TodolistsList } from "../features/TodolistsList/TodolistsList";
+import { ErrorSnackbar } from "../components/ErrorSnackbar/ErrorSnackbar";
+import { useDispatch, useSelector } from "react-redux";
+import { AppRootStateType } from "./store";
+import { initializeAppTC, RequestStatusType } from "./app-reducer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Login } from "../features/Login/Login";
+import { logoutTC } from "../features/Login/auth-reducer";
+import {
+  AppBar,
+  Button,
+  CircularProgress,
+  Container,
+  IconButton,
+  LinearProgress,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { Menu } from "@mui/icons-material";
 
-// You can learn about the difference by reading this guide on minimizing bundle size.
-// https://mui.com/guides/minimizing-bundle-size/
-// import { AppBar, Button, Container, IconButton, Toolbar, Typography } from '@mui/material';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import Container from '@mui/material/Container';
-import {CircularProgress, LinearProgress} from "@mui/material";
-import {Menu} from '@mui/icons-material';
-import {useAppDispatch, useAppSelector} from "./store";
-import {RequestStatusType} from "./app-reducer";
-import {ErrorSnackbar} from "../components/ErrorSnackbar/ErrorSnackbar";
-import {Route, Routes} from "react-router-dom";
-import {Login} from "../features/Login/Login";
-import {initializeAppTC, logoutTC} from "../features/Login/authReducer";
+type PropsType = {
+  demo?: boolean;
+};
 
+function App({ demo = false }: PropsType) {
+  const status = useSelector<AppRootStateType, RequestStatusType>((state) => state.app.status);
+  const isInitialized = useSelector<AppRootStateType, boolean>((state) => state.app.isInitialized);
+  const isLoggedIn = useSelector<AppRootStateType, boolean>((state) => state.auth.isLoggedIn);
+  const dispatch = useDispatch<any>();
 
-function App() {
+  useEffect(() => {
+    dispatch(initializeAppTC());
+  }, []);
 
-    const status = useAppSelector<RequestStatusType>((state)=>state.app.status)
-    const dispatch = useAppDispatch()
-    const isInitialized = useAppSelector((state)=> state.auth.isInitialized )
+  const logoutHandler = useCallback(() => {
+    dispatch(logoutTC());
+  }, []);
 
-    useEffect(()=>{
-        dispatch(initializeAppTC())
-    },[])
-
-    const logOut = () => {
-        dispatch(logoutTC())
-    }
-
-
-    if (!isInitialized) {
-        console.log('huy')
-        return <div
-            style={{position: 'fixed', top: '30%', textAlign: 'center', width: '100%'}}>
-            <CircularProgress/>
-        </div>
-    }
-
-
+  if (!isInitialized) {
     return (
-        <div className="App">
-            <ErrorSnackbar/>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton edge="start" color="inherit" aria-label="menu">
-                        <Menu/>
-                    </IconButton>
-                    <Typography variant="h6">
-                        News
-                    </Typography>
-                    <Button color="inherit">Login</Button>
-                    <Button color="inherit" onClick={logOut}>LogOut</Button>
-                </Toolbar>
-            </AppBar>
-            {status ==='loading' && <LinearProgress/>}
-            <Container>
-                <Routes>
-                    <Route path='/' element={<TodolistsList />}/>
-                    <Route path='/login' element={<Login/>}/>
+      <div style={{ position: "fixed", top: "30%", textAlign: "center", width: "100%" }}>
+        <CircularProgress />
+      </div>
+    );
+  }
 
-                    <Route path='*' element={<h1>404: PAGE NOT FOUND</h1>} />
-
-                </Routes>
-            </Container>
-            <Container fixed>
-
-            </Container>
-        </div>
-    )
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <ErrorSnackbar />
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" color="inherit" aria-label="menu">
+              <Menu />
+            </IconButton>
+            <Typography variant="h6">News</Typography>
+            {isLoggedIn && (
+              <Button color="inherit" onClick={logoutHandler}>
+                Log out
+              </Button>
+            )}
+          </Toolbar>
+          {status === "loading" && <LinearProgress />}
+        </AppBar>
+        <Container fixed>
+          <Routes>
+            <Route path={"/"} element={<TodolistsList demo={demo} />} />
+            <Route path={"/login"} element={<Login />} />
+          </Routes>
+        </Container>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
